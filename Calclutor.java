a61783aeb8879140487c5cf2743147c32fae5913
Shawon <khanshawon1511@gmail.com>
2023-03-29 12:54:45 +0600

    division branch added

--- Calclutor.java
+++ Calclutor.java
@@ -1,5 +1,14 @@
 public class Calclutor {
     public static void main(String[] args) {
-        Calclutor calclutor = new Calclutor();
+
+        //Calclutor calclutor = new Calclutor();
+        System.out.println(division(4,2));
+    }
+    public static double division(double a,double b){
+        if(a==0 || b==0){
+            return 0;
+        }else {
+            return a / b;
+        }
     }
 }
